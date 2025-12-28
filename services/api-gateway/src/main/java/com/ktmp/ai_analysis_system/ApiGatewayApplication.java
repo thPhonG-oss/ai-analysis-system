31b168312dc8d6b4ca9e479ad7360d3f8ef2b905
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-12-28 21:00:12 +0700

    init project

---
+++ services/api-gateway/src/main/java/com/ktmp/ai_analysis_system/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.ktmp.ai_analysis_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDiscoveryClient	
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
