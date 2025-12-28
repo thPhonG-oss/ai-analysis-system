cb629d4541761529b27755a672d2b758912129a1
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-12-28 21:10:36 +0700

    continue setup discovery-service

--- services/discovery-service/src/main/java/com/ktmp/ai_analysis_system/DiscoveryServiceApplication.java
+++ services/discovery-service/src/main/java/com/ktmp/ai_analysis_system/DiscoveryServiceApplication.java
@@ -2,14 +2,14 @@ package com.ktmp.ai_analysis_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient	
-public class ApiGatewayApplication {
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
 }
---
+++ services/discovery-service/src/test/java/com/ktmp/ai_analysis_system/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktmp.ai_analysis_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
