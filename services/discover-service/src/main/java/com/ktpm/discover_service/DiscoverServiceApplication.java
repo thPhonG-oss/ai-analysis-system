d216d3df1e6a154566c4c4975a54b72ded93075b
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-12-28 21:27:53 +0700

    downgarde spring boot version

--- services/discover-service/src/main/java/com/ktpm/discover_service/DiscoverServiceApplication.java
+++ services/discover-service/src/main/java/com/ktpm/discover_service/DiscoverServiceApplication.java
@@ -1,13 +1,13 @@
-package com.ktpm.discovery_service;
+package com.ktpm.discover_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiscoveryServiceApplication {
+public class DiscoverServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServiceApplication.class, args);
+		SpringApplication.run(DiscoverServiceApplication.class, args);
 	}
 
 }
--- services/discover-service/src/test/java/com/ktpm/discover_service/DiscoverServiceApplicationTests.java
+++ services/discover-service/src/test/java/com/ktpm/discover_service/DiscoverServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ktpm.discovery_service;
+package com.ktpm.discover_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryServiceApplicationTests {
+class DiscoverServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
